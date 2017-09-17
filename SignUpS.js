cd675501cc620bb2ad5094c2d12435814c063fc6
Charlotte Weir <weirchar@c-04-06-lab.mddn.vuw.ac.nz>
2017-09-18 10:00:22 +1200

    Init commit

---
+++ SignUpS.js
@@ -0,0 +1,11 @@
+
+function swapImage(id,primary,secondary) {
+    src=document.getElementById(id).src;
+    if (src.match(primary)) {
+      document.getElementById(id).src=secondary;
+    } else {
+      document.getElementById(id).src=primary;
+    }
+  }
+
+
